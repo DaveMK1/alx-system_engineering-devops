2622a763a7d0d92281a7edf7cdd799af3c15b042
David Agbesinyale <davidm7040@gmail.com>
2024-05-07 14:01:39 +0000

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that queries subscribers on a given Reddit subreddit """
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """  Return the number of subscribers on a given subreddit"""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
