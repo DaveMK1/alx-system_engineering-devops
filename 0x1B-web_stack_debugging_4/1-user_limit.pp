000827650f463f03709855a8fc74e9fdab353ce4
David Agbesinyale <davidm7040@gmail.com>
2024-05-15 08:30:44 -0700

    done

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Fix problem of high amount files opened
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
